9dab76950d1d31ab75b994836e9fb0cc0c852602
Lance Paine <lance@lbpaine.com>
2017-05-11 08:50:48 +0100

    ignore resources/js/out

--- resources/public/js/out/figwheel/connect/build_drtsim.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-drtsim (:require [quilcljd_sim.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "drtsim", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
