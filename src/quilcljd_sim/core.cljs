2376661db36f3a3d4b905505352f0bb9cc7dbcfa
lance paine <lance@lbpaine.com>
2017-05-15 18:58:16 +0100

    adding people to persistent queue

--- src/quilcljd_sim/core.cljs
+++ src/quilcljd_sim/core.cljs
@@ -2,6 +2,13 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(defmethod print-method clojure.lang.PersistentQueue [q, w] ; Overload the print for queues
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
+
+(def pqe clojure.lang.PersistentQueue/EMPTY)
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
@@ -12,12 +19,13 @@
   ; circle color and position.
   {:color 0
    :angle 5
-   :queue [10 30 20 10 50 60 10 1]
+   :queue (conj pqe 40 10 30 100 20 10)
    :desks [200 110 50 20 0 50 20] 
    })
 
 (def desk-width 40)
 (def desk-gap 5)
+
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
@@ -33,15 +41,50 @@
      (let [space (+ desk-gap desk-width)]
         (q/fill 55 n 255)
         (q/rect (* i space) 20 desk-width desk-width)))
-   desks
-   )))
+   desks)))
 
-(defn draw-state [state]
+(let [q (conj pqe 1 2 3 4)]
+  (doall (map #(+ 1 %1) q)))
+
+(defn pq [s]
+ (apply conj clojure.lang.PersistentQueue/EMPTY s))
+
+(conj pqe  1 2 3 )
+
+(def schedule
+  (conj clojure.lang.PersistentQueue/EMPTY
+        :wake-up :shower :brush-teeth))
+
+(def people
+  (conj clojure.lang.PersistentQueue/EMPTY
+        10 20 10 40 100
+        ))
+
+people
+
+(== 4 (.indexOf [1 2 3 4 0] 0))
+(== 3 (.indexOf [1 2 3 4 0] 4))
+(== 0 (.indexOf [1 2 3 4 0] 1))
+(== 0 (.indexOf [1 2 3 4 0] 9))
+(.indexOf [1 2 3 4 0] 9)
+
+(let [d [30 2 0 10]
+      q (pq (99 22 30))
+      first-zero (.indexOf d 0)
+      qhead (peek q)
+      ]
+  (println d first-zero qhead)
+  (assoc d first-zero qhead)
+  )
+
+
+
+        (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
   ; Set circle color.
-  (let [desks (:desks state)]
-    (draw-desks desks))
+  (q/with-translation [10 400]
+    (draw-desks (:desks state)))
   ;; (let [a 1]
   ;;   (q/fill 210 120 220)
   ;;   (q/rect 2 20 40 40))
